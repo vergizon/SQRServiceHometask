f4218df77e859980ec0d513d16d640633bcc8b68
Alex Vergizov <alvergizoff@gmail.com>
2022-08-02 23:02:37 +0300

    SCRService corrected

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -3,9 +3,7 @@ package ru.netology.sqr;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 public class SQRServiceTest {
 
